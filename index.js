2a2a0205f4bc6b4479158d899f216eeac2bd2a85
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 20:17:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById("app").querySelectorAll("ul.rank-list li")
+  var rank = document.getElementById("app").querySelectorAll("ul.rank-list li")
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML + 1)
   }
