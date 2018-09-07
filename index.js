7925ca7ebeb22314b9fb72259eaf5484bf426741
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 20:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 function getFirstSelector(selector) {
-  return document.querySelector(selector);
+  return document.querySelector(selector)
 }
 
 function nestedTarget() {
-  return document.getElementById("nested").querySelector(".target");
+  return document.getElementById('nested').querySelector('.target')
 }
 
 function increaseRankBy(n) {
-  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
-  for(let i=0;i<lis.length;i++) {
-    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  const lis = document.getElementById("app").querySelectorAll("ul.rank-list li")
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML + 1)
   }
-}
-
-function deepestChild() {
-  var lis=document.getElementById("grand-node").querySelectorAll("div");
-  var test;
-  for(let i=0;i<lis.length-1;i++) {
-    test=lis[i].querySelector("div");
-  }
-return test;
 }
